a9ff2893000ff34f8d681b81734533d22116b52d
Ibsa Milkeso <milkesoibsa@gmail.com>
2024-09-24 18:53:31 +0300

    05_Sumall code passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let summation = 0;
+    let c=0;
+    if(a<0 || b<0 || !Number.isInteger(a) || !Number.isInteger(b) || typeof a !=='number' || typeof b !=='number') return "ERROR";
+    if (a>b) {
+        c = a; a = b; b = c;
+    }
+    for(let i=a; i<=b; i++){
+        summation+=i;
+    }
+    return summation;
 };
 
 // Do not edit below this line
