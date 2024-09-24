54479585accd5d04c4dd92be3adbe2e361d28173
Ibsa Milkeso <milkesoibsa@gmail.com>
2024-09-24 18:20:59 +0300

    04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...rest) {
+    let newArray = [];
+    for(let i=0; i<inputArray.length; i++){
+        if (!rest.includes(inputArray[i])) {
+            newArray.push(inputArray[i]);
+        }
+    }
+    return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
