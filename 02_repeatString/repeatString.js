f15e60b35214abf63555e63128ccda8571715058
Ibsa Milkeso <milkesoibsa@gmail.com>
2024-09-24 11:34:21 +0300

    02_repeatString.js Code pass all test cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    let resultText="";
+    if(num<0){
+        return 'ERROR';
+    }
+    for(let i=0; i<num; i++){
+        resultText+=text;
+    }
+    return resultText;
 };
 
 // Do not edit below this line
