42b6ceeead0b27595fde01126d810afae89ef1c3
Ibsa Milkeso <milkesoibsa@gmail.com>
2024-09-25 20:11:52 +0300

    07_tempConversion.spec.js code passes all test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  //°C = (°F - 32) × 5/9
+  let tempInCelsius = (temp-32)*(5/9);
+  //var rounded = Math.round(number * 10) / 10
+  let roundedCelcius = Math.round(tempInCelsius*10)/10;
+  return roundedCelcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  //°F = (9/5) °C+32
+  let tempInFahrenheit = (9/5)*temp+32;
+  let roundedFahrenheit = Math.round(tempInFahrenheit*10)/10;
+  return roundedFahrenheit;
 };
 
 // Do not edit below this line
