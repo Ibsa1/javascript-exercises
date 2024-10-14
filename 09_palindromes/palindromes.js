dd8e068b8c81bc0858b49a0e661878880fb71379
Ibsa Milkeso <milkesoibsa@gmail.com>
2024-10-14 11:03:13 +0300

    Solution for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (myString) {
+    let myArray = myString.toLowerCase().replace(/[^a-z0-9]/g, '').split('');
 
+    for (let i=0, j=myArray.length-1; i<j; i++, j--){
+        if(myArray[i]!==myArray[j]){
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
