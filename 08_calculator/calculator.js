9b85d725f47d12d5301642e96a11b45e6ea1e85a
Ibsa Milkeso <milkesoibsa@gmail.com>
2024-10-14 10:29:18 +0300

    Solution for 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let acc = 0;
+  for(let i=0; i<arr.length; i++){
+    acc += arr[i];
+  }
+  return acc;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let acc = 1;
+  for(let i=0; i<arr.length; i++){
+    acc *= arr[i];
+  }
+  return acc;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if(a == 0 ){
+    return 1;
+  }
+  else{
+    let acc = 1;
+    for (let i=1; i<=a; i++){
+      acc *= i;
+    }
+    return acc;
+  }
 };
 
 // Do not edit below this line
